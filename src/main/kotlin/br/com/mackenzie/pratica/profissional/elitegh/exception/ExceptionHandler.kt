f2a07c289ef80b431deceeccd9306273904cb1f3
Gustavo <gustavo.macedo0202@gmail.com>
2023-10-23 19:05:28 -0300

    feat: adjust return to exception

--- src/main/kotlin/br/com/mackenzie/pratica/profissional/elitegh/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/mackenzie/pratica/profissional/elitegh/exception/ExceptionHandler.kt
@@ -1,6 +1,7 @@
 package br.com.mackenzie.pratica.profissional.elitegh.exception
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -11,10 +12,16 @@ class ExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleUserNotFoundException(ex: UserNotFoundException) = ex.message
+    fun handleUserNotFoundException(ex: UserNotFoundException): ResponseEntity<Any> {
+        val response = mapOf("message" to ex.message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
+    }
 
     @ExceptionHandler(CourseNotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleCourseNotFoundException(ex: CourseNotFoundException) = ex.message
+    fun handleCourseNotFoundException(ex: CourseNotFoundException): ResponseEntity<Any> {
+        val response = mapOf("message" to ex.message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
+    }
 
 }
